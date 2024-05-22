61080e8244a603b4378f034650d8d02c77e73d42
Julia Kardasz <juliakardasz.jk@gmail.com>
2024-05-22 20:58:47 +0200

    cleaner code

--- src/dynamicMethod.cpp
+++ src/dynamicMethod.cpp
@@ -2,35 +2,26 @@
 
 #include "../include/object_struct.h"
 
-void dynamicMethod(object *backpack, int N, int C){
-
-    // object backpack[4];
-    // int C = 8;
-    // backpack[0].w = 2; backpack[0].p = 4;
-    // backpack[1].w = 1; backpack[1].p = 3;
-    // backpack[2].w = 4; backpack[2].p = 6;
-    // backpack[3].w = 4; backpack[3].p = 8;
-    int matrix[N+1][C+1];
-    for(int i = 0; i < N+1; i++){
-        for(int j = 0; j < C+1; j++) matrix[i][j] = 0;
-    }
+void findResult(object *backpack, int **costMatrix, int N, int C) {
     int a;
-    for(int i = 1; i < N+1; i++){
-        for(int j = 1; j < C+1; j++){
+    for(int i = 1; i < N+1; i++) {
+        for(int j = 1; j < C+1; j++) {
             a = i-1;
-            //std::cout << "a: " << a << " ";
-            //std::cout << backpack[a].w << " " << backpack[a].p << "\n";
-            if(backpack[a].w > j) matrix[i][j] = matrix[i-1][j]; //jak sie nie misci, to brac to co bylo o jedno wyzej
-            else{ //jak sie miesci to wpakuj, jesli jest wieksze po dodaniu tego elementu
-                matrix[i][j] = std::max(matrix[i-1][j], matrix[i-1][j-backpack[a].w] + backpack[a].p);
+            if(backpack[a].w > j) {
+                costMatrix[i][j] = costMatrix[i-1][j];
+            }
+            else{
+                costMatrix[i][j] = std::max(costMatrix[i-1][j], costMatrix[i-1][j-backpack[a].w] + backpack[a].p);
             }
         }   
     }
+}
 
-    std::cout << "Max value of backpack: " << matrix[N][C] << "\nPacked elements: ";
+void displayResult(object *backpack, int **costMatrix, int N, int C) {
+    std::cout << "Max value of backpack: " << costMatrix[N][C] << "\nPacked elements: ";
     int i = N, j = C;
     while(i > 0 && j > 0){
-            if(matrix[i][j] != matrix[i-1][j]){
+            if(costMatrix[i][j] != costMatrix[i-1][j]) {
                 std::cout << i << " ";
                 i -= 1;
                 j -= backpack[i-1].w;
@@ -38,4 +29,33 @@ void dynamicMethod(object *backpack, int N, int C){
             else i -= 1;
     }
     std::cout << "\n";
+}
+
+void dynamicMethod(object *backpack, int N, int C) {
+
+    // object backpack[4];
+    // int C = 8;
+    // backpack[0].w = 2; backpack[0].p = 4;
+    // backpack[1].w = 1; backpack[1].p = 3;
+    // backpack[2].w = 4; backpack[2].p = 6;
+    // backpack[3].w = 4; backpack[3].p = 8;
+    int **costMatrix;
+    costMatrix = new int*[N+1];
+    for(int i = 0; i < N+1; i++) {
+        costMatrix[i] = new int[C+1];
+    }
+
+    for(int i = 0; i < N+1; i++) {
+        for(int j = 0; j < C+1; j++) {
+            costMatrix[i][j] = 0;
+        }
+    }
+
+    findResult(backpack, costMatrix, N, C);
+    displayResult(backpack, costMatrix, N, C);
+
+    for (int i = 0; i < N+1; i++) {
+        delete[] costMatrix[i];
+    }
+    delete[] costMatrix; 
 }
